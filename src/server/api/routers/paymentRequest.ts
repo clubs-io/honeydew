0634296e586201f756352cfe3084333664365a30
neilk17 <kanakianeil@gmail.com>
2023-05-18 18:08:59 -0400

    changes (temporary)

--- src/server/api/routers/paymentRequest.ts
+++ src/server/api/routers/paymentRequest.ts
@@ -2,68 +2,75 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { z } from "zod";
 
-import {
-createTRPCRouter,
-protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const paymentRequestRouter = createTRPCRouter({
-    createPaymentRequest: protectedProcedure
-        .input(
-            z.object({
-            user_id: z.string(),
-            organization_id: z.number(),
-            amount: z.number(),
-            due_by: z.date(),
-          }),
-        ).mutation(async ({ ctx, input }) => {
-            try {
-                const userToRequest = await ctx.prisma.user.findUnique({
-                    where:{
-                      id: input.user_id,
-                    }
-                  });
-              if (userToRequest && userToRequest.organizationId)
-              await ctx.prisma.paymentRequest.create({
-                  data: {
-                    userId: input.user_id,
-                    amount: input.amount,
-                    dueBy: input.due_by,
-                    amountPending: input.amount,
-                    organizationId: userToRequest.organizationId,
-                  },
-              }).catch((e: any) => { console.error('Failed to create payment request'); });   
-            } catch (error) {
-              console.log("error", error);
-            }
-          }),
-    fulfillPaymentRequest: protectedProcedure
-        .input(
-            z.object({
-            user_id: z.string(),
-            organization_id: z.number(),
-            amount: z.number(),
-            due_by: z.date(),
-          }),
-        ).mutation(async ({ ctx, input }) => {
-            try {
-                const userToRequest = await ctx.prisma.user.findUnique({
-                    where:{
-                      id: input.user_id,
-                    }
-                  });
-              if (userToRequest && userToRequest.organizationId)
-              await ctx.prisma.paymentRequest.create({
-                  data: {
-                    userId: input.user_id,
-                    amount: input.amount,
-                    dueBy: input.due_by,
-                    amountPending: input.amount,
-                    organizationId: userToRequest.organizationId,
-                  },
-              }).catch((e: any) => { console.error('Failed to fulfill request'); });   
-            } catch (error) {
-              console.log("error", error);
-            }
-          })
-})
+  createPaymentRequest: protectedProcedure
+    .input(
+      z.object({
+        user_id: z.string(),
+        organization_id: z.string(),
+        amount: z.number(),
+        due_by: z.nullable(z.date()),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const userToRequest = await ctx.prisma.user.findUnique({
+          where: {
+            id: input.user_id,
+          },
+        });
+        if (userToRequest && userToRequest.organizationId && input.due_by)
+          return await ctx.prisma.paymentRequest
+            .create({
+              data: {
+                userId: input.user_id,
+                amount: input.amount,
+                dueBy: input.due_by,
+                amountPending: input.amount,
+                organizationId: userToRequest.organizationId,
+              },
+            })
+            .catch((e: any) => {
+              console.error("Failed to create payment request");
+            });
+      } catch (error) {
+        console.log("error", error);
+      }
+    }),
+  fulfillPaymentRequest: protectedProcedure
+    .input(
+      z.object({
+        user_id: z.string(),
+        organization_id: z.number(),
+        amount: z.number(),
+        due_by: z.date(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const userToRequest = await ctx.prisma.user.findUnique({
+          where: {
+            id: input.user_id,
+          },
+        });
+        if (userToRequest && userToRequest.organizationId)
+          await ctx.prisma.paymentRequest
+            .create({
+              data: {
+                userId: input.user_id,
+                amount: input.amount,
+                dueBy: input.due_by,
+                amountPending: input.amount,
+                organizationId: userToRequest.organizationId,
+              },
+            })
+            .catch((e: any) => {
+              console.error("Failed to fulfill request");
+            });
+      } catch (error) {
+        console.log("error", error);
+      }
+    }),
+});
