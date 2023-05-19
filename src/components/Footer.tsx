d2b37d857a96a802e00e240cc9b7b24aa8a6ca66
neilk17 <kanakianeil@gmail.com>
2023-05-18 20:33:01 -0400

    landing page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,50 +1,61 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 
 const Footer = () => {
   const Items = [
     {
       title: "About",
-      link: "#"
+      link: "#",
     },
     {
       title: "Discord Server",
-      link: "#"
+      link: "#",
     },
     {
       title: "Contact Us",
-      link: "#"
+      link: "#",
     },
     {
       title: "Terms",
-      link: "#"
+      link: "#",
     },
-  ]
+  ];
   return (
     <div className="bg-white dark:bg-gray-800">
-        <div className="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
-            <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
-                <div>
-                    <h3 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h3>
-                    <ul className="text-gray-500 dark:text-gray-400">
-                      {Items.map((item, index) => (
-                        <li className="mb-4" key={index}>{item.title}</li>
-                      ))}
-                    </ul>
-                </div>
-            </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <div className="text-center">
-            <Link href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
-                {/* <Image src="./images/logo.svg" fill class="h-6 mr-3 sm:h-9" alt="Landwind Logo" /> */}
-                Honeydew    
-            </Link>
-            <span className="block text-sm text-center text-gray-500 dark:text-gray-400">© 2021-2022 Honeydew. All Rights Reserved. Built with 
-            <Link href="https://github.com/themesberg/landwind" className="text-purple-600 hover:underline dark:text-purple-500"> Landwind </Link>
-            </span>
-          </div>
+      <div className="mx-auto max-w-screen-xl p-4 py-6 md:p-8 lg:p-10 lg:py-16">
+        <div>
+          <h3 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+            Company
+          </h3>
+          <ul className="flex flex-row gap-8 text-gray-500 dark:text-gray-400">
+            {Items.map((item, index) => (
+              <li className="mb-4" key={index}>
+                {item.title}
+              </li>
+            ))}
+          </ul>
         </div>
+        <hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
+        <div className="text-center">
+          <Link
+            href="#"
+            className="mb-5 flex items-center justify-center text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            {/* <Image src="./images/logo.svg" fill class="h-6 mr-3 sm:h-9" alt="Landwind Logo" /> */}
+            Honeydew
+          </Link>
+          <span className="block text-center text-sm text-gray-500 dark:text-gray-400">
+            © 2021-2022 Honeydew. All Rights Reserved. Built with
+            <Link
+              href="https://github.com/themesberg/landwind"
+              className="text-purple-600 hover:underline dark:text-purple-500"
+            >
+              {" "}
+              Landwind{" "}
+            </Link>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import splash from "../assets/splash.svg";
-import illustration from "../assets/3d-illustration.png";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 
@@ -9,22 +7,26 @@ export function HeroBanner() {
   return (
     <div className="bg-white dark:bg-gray-900">
       {!(sessionData?.user !== undefined) && (
-        <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+        <div className="mx-auto grid max-w-screen-xl px-4 pb-8 pt-20 lg:grid-cols-12 lg:gap-8 lg:py-16 lg:pt-28 xl:gap-0">
           <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
               Financial solutions for your college community
             </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              Our platform provides a comprehensive suite of tools to manage expenses, dues, and fundraising, all in one place. With our easy-to-use interface and powerful financial reporting capabilities, you can easily stay on top of your fraternity&apos;s financial health
+            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
+              Our platform provides a comprehensive suite of tools to manage
+              expenses, dues, and fundraising, all in one place. With our
+              easy-to-use interface and powerful financial reporting
+              capabilities, you can easily stay on top of your fraternity&apos;s
+              financial health
             </p>
           </div>
-          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <div className="hidden lg:col-span-5 lg:mt-0 lg:flex">
             <Image
-              src="/hero.png"
+              src="/dancing.svg"
               alt="Honeydew"
               width={1064}
               height={832}
-              />
+            />
           </div>
         </div>
       )}
