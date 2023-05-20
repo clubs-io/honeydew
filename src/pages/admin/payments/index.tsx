8258e09bdbec2b3e48800dc446022fed495d8c1a
neilk17 <kanakianeil@gmail.com>
2023-05-20 16:50:26 -0400

    Modal UI

--- src/pages/admin/payments/index.tsx
+++ src/pages/admin/payments/index.tsx
@@ -12,6 +12,7 @@ import {
   ArrowUpIcon,
   UserPlusIcon,
   PlusIcon,
+  CurrencyDollarIcon,
 } from "@heroicons/react/24/outline";
 import { Dialog, Transition } from "@headlessui/react";
 import Select from "react-select";
@@ -183,14 +184,14 @@ const AdminPayments: NextPage = () => {
               <div className="h-full w-full">
                 {/* Header */}
                 <div className="flex flex-col justify-between sm:flex-row">
-                    <div>
-                      <h1 className="text-4xl font-medium text-slate-800 dark:text-slate-100 sm:block">
-                        Track Payments
-                      </h1>
-                      <p className="text-xl text-slate-500">
-                        Track and manage your Organization&apos;s Payments
-                      </p>
-                      {/* <div>
+                  <div>
+                    <h1 className="text-4xl font-medium text-slate-800 dark:text-slate-100 sm:block">
+                      Track Payments
+                    </h1>
+                    <p className="text-xl text-slate-500">
+                      Track and manage your Organization&apos;s Payments
+                    </p>
+                    {/* <div>
                         {!isLoading && subscriptionStatus !== null && (
                           <>
                             <p>Sub: {subscriptionStatus?.stripeSubscriptionStatus}</p>
@@ -206,7 +207,7 @@ const AdminPayments: NextPage = () => {
                           </>
                         )}
                       </div> */}
-                    </div>
+                  </div>
                   <div>
                     <button
                       type="button"
@@ -286,30 +287,36 @@ const AdminPayments: NextPage = () => {
                   >
                     <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                       <form className="" onSubmit={mutateCreatePaymentRequest}>
-                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                        <div className="px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                           <div className="sm:flex sm:items-start">
                             <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
-                              <UserPlusIcon
+                              <CurrencyDollarIcon
                                 className="h-6 w-6 text-blue-600"
                                 aria-hidden="true"
                               />
                             </div>
-                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                              <Dialog.Title
-                                as="h3"
-                                className="text-base font-semibold leading-6 text-gray-900"
-                              >
-                                Payment Request Form
-                              </Dialog.Title>
-                              <div className="mt-2">
+                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left w-full">
+                              <div>
+                                <Dialog.Title
+                                  as="h3"
+                                  className="text-base font-semibold leading-6 text-gray-900"
+                                >
+                                  Payment Request Form
+                                </Dialog.Title>
                                 <p className="text-sm text-gray-500">
                                   Create a payment request from a user.
+                                </p>
+                                <hr className="my-4 h-px w-full border-0 bg-gray-200" />
+                              </div>
+                              <div className="">
+                                <p className="text-sm text-gray-800">
+                                  Select user
                                 </p>
                                 <Select
                                   defaultValue={[]}
                                   name="members"
                                   options={options}
-                                  className="basic-multi-select"
+                                  className="basic-multi-select mt-1"
                                   classNamePrefix="select"
                                   //   onChange={}
                                   onChange={
@@ -319,29 +326,38 @@ const AdminPayments: NextPage = () => {
                                     // console.log(e);
                                   }
                                 />
-
-                                <p>Amount To Be Requested</p>
-                                <input
-                                  type="text"
-                                  name="price"
-                                  id="price"
-                                  className="mt-4 block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                  placeholder="10"
-                                  value={paymentAmountValue}
-                                  onChange={(e) =>
-                                    setPaymentAmountValue(e.target.value)
-                                  }
-                                />
-                                <p>Due Date</p>
-                                <input
-                                  type="date"
-                                  name="price"
-                                  id="date"
-                                  className="mt-4 block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                  placeholder="May 11th"
-                                  value={paymentDate?.toString()}
-                                  onChange={handleDateChange}
-                                />
+                                <hr className="my-4 h-px border-0 bg-gray-200 dark:bg-gray-700" />
+                                <div className="">
+                                  <p className="text-sm text-gray-800">
+                                    Enter amount
+                                  </p>
+                                  <input
+                                    type="text"
+                                    name="price"
+                                    id="price"
+                                    className="mt-1 block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    placeholder="10"
+                                    value={paymentAmountValue}
+                                    onChange={(e) =>
+                                      setPaymentAmountValue(e.target.value)
+                                    }
+                                  />
+                                </div>
+                                <hr className="my-4 h-px border-0 bg-gray-200 dark:bg-gray-700" />
+                                <div className="">
+                                  <p className="text-sm text-gray-800">
+                                    Due by
+                                  </p>
+                                  <input
+                                    type="date"
+                                    name="price"
+                                    id="date"
+                                    className="mt-1 block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    placeholder="May 11th"
+                                    value={paymentDate?.toString()}
+                                    onChange={handleDateChange}
+                                  />
+                                </div>
                               </div>
                             </div>
                           </div>
@@ -349,14 +365,14 @@ const AdminPayments: NextPage = () => {
                         <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                           <button
                             type="submit"
-                            className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+                            className="px-10 inline-flex w-full justify-center rounded-md bg-blue-600 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
                             onClick={() => setOpen(false)}
                           >
-                            Invite
+                            Create
                           </button>
                           <button
                             type="button"
-                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                            className=" inline-flex w-full justify-center rounded-md bg-white px-10 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                             onClick={() => setOpen(false)}
                             ref={cancelButtonRef}
                           >
