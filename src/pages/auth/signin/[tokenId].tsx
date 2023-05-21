7ea9145bd371920d51cb851a8d60071b0d058086
neilk17 <kanakianeil@gmail.com>
2023-05-21 17:28:58 -0400

    signup page for invited users

--- src/pages/auth/signin/[tokenId].tsx
+++ src/pages/auth/signin/[tokenId].tsx
@@ -1,34 +1,47 @@
-import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
-import { getProviders, signIn } from "next-auth/react"
-import { getServerSession } from "next-auth/next"
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import { getProviders, signIn } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "~/server/auth";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function SignInWithInvite({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function SignInWithInvite({
+  providers,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
   const { tokenId } = router.query;
   return (
-    <div className="bg-slate-50 dark:bg-gray-900 h-screen w-screen flex p-36 items-center justify-center">
-      <div className="dark:bg-gray-800 bg-white shadow w-fit p-8 rounded-md">
-        <h2 className="text-4xl text-slate-800 font-extrabold dark:text-white">Welcome back</h2>
+    <div className="flex h-screen w-screen items-center justify-center bg-slate-50 p-36 dark:bg-gray-900">
+      <div className="w-fit rounded-md bg-white p-8 shadow dark:bg-gray-800">
+        <h2 className="text-4xl font-extrabold text-slate-800 dark:text-white">
+          Welcome to honeydew
+        </h2>
+        <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+          Click here to setup your account
+        </p>
         {Object.values(providers).map((provider) => (
           <div key={provider.name}>
-            <button className="mt-8 w-full text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            <button
+              className="mb-2 mr-2 mt-8 w-full rounded-lg bg-blue-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               // eslint-disable-next-line @typescript-eslint/no-misused-promises, @typescript-eslint/restrict-plus-operands
-              onClick={() => signIn(provider.id, {callbackUrl: process.env.NEXT_PUBLIC_URL + "/accept_invite/" + tokenId})}>
+              onClick={() =>
+                signIn(provider.id, {
+                  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                  callbackUrl: `${process.env.NEXT_PUBLIC_URL}/accept_invite/${tokenId}`,
+                })
+              }
+            >
               Sign in with {provider.name}
             </button>
           </div>
         ))}
-        <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-        <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-          Don&apos;t have an account yet?
-          <Link className="text-blue-500 hover:underline dark:text-primary-500" href="#"> Sign-up here</Link>
-        </p>
+        <hr className="my-8 h-px border-0 bg-gray-200 dark:bg-gray-700" />
       </div>
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
@@ -45,5 +58,5 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   return {
     props: { providers: providers ?? [] },
-  }
+  };
 }
