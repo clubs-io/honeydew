eda58021beefa958a3a653e5320a26eb1c2c0a6f
neilk17 <kanakianeil@gmail.com>
2023-05-21 16:35:29 -0400

    calendar UI

--- src/pages/admin/calendar.tsx
+++ src/pages/admin/calendar.tsx
@@ -16,44 +16,45 @@ const Calendar: NextPage = () => {
   const { data: sessionData, status } = useSession();
   const router = useRouter();
   const [calendarLink, setCalendarLink] = useState<string>("");
-  
+
   const currentUserOrganization = api.user?.getUserOrganization.useQuery(
     sessionData?.user.id ? sessionData?.user.id : ""
   );
 
-  const getOrganizationCalendarLink = api.organization?.getCalendarLink.useQuery({
-    org_id: currentUserOrganization.data?.organizationId ?? ""
-  })
+  const getOrganizationCalendarLink =
+    api.organization?.getCalendarLink.useQuery({
+      org_id: currentUserOrganization.data?.organizationId ?? "",
+    });
 
   const utils = api.useContext();
   let optimisticUpdate = null;
 
-  const setOrganizationCalendarLink = api.organization?.setCalendarLink.useMutation({
-    // When mutate is called:
-    onMutate: () => {
-      // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
-      void utils.organization.getCalendarLink.cancel();
-      // Snapshot the previous value
-      optimisticUpdate = utils.organization.getCalendarLink.getData();
-      // Optimistically update to the new value
-      if (optimisticUpdate) {
-        utils.organization.getCalendarLink.setData(
-          {org_id: currentUserOrganization.data?.organizationId ?? ""},
-          optimisticUpdate
-        );
-      }
-    },
-    // todo: need to add error case
-    // Always refetch after error or success:
-    onSettled: () => {
-      void utils.organization.getCalendarLink.invalidate();
-    },
-  });
+  const setOrganizationCalendarLink =
+    api.organization?.setCalendarLink.useMutation({
+      // When mutate is called:
+      onMutate: () => {
+        // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+        void utils.organization.getCalendarLink.cancel();
+        // Snapshot the previous value
+        optimisticUpdate = utils.organization.getCalendarLink.getData();
+        // Optimistically update to the new value
+        if (optimisticUpdate) {
+          utils.organization.getCalendarLink.setData(
+            { org_id: currentUserOrganization.data?.organizationId ?? "" },
+            optimisticUpdate
+          );
+        }
+      },
+      // todo: need to add error case
+      // Always refetch after error or success:
+      onSettled: () => {
+        void utils.organization.getCalendarLink.invalidate();
+      },
+    });
 
   useEffect(() => {
-    setCalendarLink(getOrganizationCalendarLink.data?.calendarLink ?? "")
+    setCalendarLink(getOrganizationCalendarLink.data?.calendarLink ?? "");
   }, [getOrganizationCalendarLink.isSuccess]);
-
 
   if (status === "unauthenticated") {
     void router.push("/");
@@ -66,37 +67,46 @@ const Calendar: NextPage = () => {
           <main className="flex min-h-screen w-screen flex-row justify-center bg-slate-50 dark:bg-slate-700">
             <div className="container mx-16 mt-12 max-w-7xl gap-12">
               <div className="h-full w-full">
-              <p>Calendar Link</p>
-              <input
-                type="text"
-                className="mt-4 block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="Outlook or GCal Calendar Link"
-                value={calendarLink}
-                onChange={(e) =>
-                  setCalendarLink(e.target.value)
-                }
-              />
-              <button
-                type="submit"
-                className="mt-4 inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:w-auto"
-                onClick={() => {
-                  setOrganizationCalendarLink.mutate({
-                    org_id: currentUserOrganization.data?.organizationId ?? "",
-                    calendar_link: calendarLink
-                  })
-                }}
-              >
-                Connect Calendar
-              </button>
-              {
-                calendarLink.length > 4 ? 
-              (<iframe src={calendarLink}
-                className="w-3/4 h-3/4"
-              ></iframe>
-              ) : null
-              }
+                <div className="flex flex-col justify-between sm:flex-row">
+                  <div>
+                    <h1 className="text-4xl font-medium text-slate-800 dark:text-slate-100 sm:block">
+                      Organization Calendar
+                    </h1>
+                    <p className="text-xl text-slate-500">
+                      View your Organization&apos;s calendar and events
+                    </p>
+                  </div>
+                  <div>
+                    <button
+                      type="button"
+                      className="inline-flex w-full items-center justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+                      onClick={() => {
+                        setOrganizationCalendarLink.mutate({
+                          org_id:
+                            currentUserOrganization.data?.organizationId ?? "",
+                          calendar_link: calendarLink,
+                        });
+                      }}
+                    >
+                      Connect Calendar
+                    </button>
+                  </div>
+                </div>
+                <input
+                  type="text"
+                  className="mt-4 block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Outlook or GCal Calendar Link"
+                  value={calendarLink}
+                  onChange={(e) => setCalendarLink(e.target.value)}
+                />
+                {calendarLink.length > 4 ? (
+                  <iframe
+                    src={calendarLink}
+                    className="h-3/4 sm:w-full"
+                  ></iframe>
+                ) : null}
+              </div>
             </div>
-          </div>
           </main>
         </div>
       </>
